b9748ec2fd4faa5fac212f32518cf368ec802eee
TmSalviano <TiagoMSalviano@outlook.com>
2025-10-14 17:29:12 -0300

    string in operator

--- string.py
+++ string.py
@@ -1,19 +1,2 @@
 
-
-def counter(word: str, letter: str):
-    """
-        takes a word and counts the amount of ocurrences for a certain letter
-        Args:
-            word (str): the word to iterate over
-            letter (str): the letter to count
-        Returns:
-            int: the total number of ocurrances for that letter in a word
-    """
-    count = 0
-    for char in word:
-        if char == letter:
-            count += 1
-    return count
-            
-    
-print(counter("banana", "a"))
+print("ana" in "banana")
