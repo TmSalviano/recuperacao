b0c7991938f84c6ec7905dfe553c2c497248edd5
TmSalviano <TiagoMSalviano@outlook.com>
2025-10-14 14:37:36 -0300

    mad_libs

---
+++ mad_libs.py
@@ -0,0 +1,14 @@
+
+print("Give me the words to complete the sentence: \n")
+adjective = input("Give me an adjective: ")
+noun = input("Give me an noun: ")
+adverb = input("Give me an adverb: ")
+verb = input("Give me an verb: ")
+place = input("Give me a place: ")
+food = input("Give me a food: ")
+
+
+sentence = f"""Last night, I dreamed of a {adjective} {noun}. 
+It was {adverb} {verb}ing around the {place} while eating a {food}."""
+
+print(sentence)
