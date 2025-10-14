344a8f800326521bb45f3d210d0e861263717b51
TmSalviano <TiagoMSalviano@outlook.com>
2025-10-13 21:51:16 -0300

    hacking

--- fahrenheit.py
+++ fahrenheit.py
@@ -1,4 +1,9 @@
 
-family = ["Ronaldo", "Mary", "Giovana"]
-for person in family:
-    print(f"Happy birthday: {person}")
+maximum = None
+print(f"Max: ", maximum)
+
+for itervar in [0, 3, 25, 23, 21, 42]:
+    if maximum is None or itervar > maximum:
+        maximum = itervar
+        print(f"New Maximum: {maximum}")
+print(f"Overall Maximum: {maximum}")
