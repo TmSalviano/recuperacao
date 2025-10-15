10091821bd4530a0fe508dd7330e0feb4fcb07ee
TmSalviano <TiagoMSalviano@outlook.com>
2025-10-15 19:24:33 -0300

    change: lists.py -> calculate average from list

--- lists.py
+++ lists.py
@@ -1,9 +1,9 @@
 
-l = ['a', 'b', 'c']
-another_list = ['d', 'e', 'f']
-another_list.extend(l)
-another_list.sort()
+l = []
 
-b_item = another_list.pop(1)
-print(another_list)
-print("Here is the letter b: " + b_item)
+while True:
+    num = input()
+    if num == "done":
+        print("Average: ", (sum(l) / len(l)))
+        break 
+    l.append(float(num))
