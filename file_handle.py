9f5e9233ab24c1797ec91280db10b11afaa913b8
TmSalviano <TiagoMSalviano@outlook.com>
2025-10-15 15:56:06 -0300

    operations with user defined files

--- file_handle.py
+++ file_handle.py
@@ -1,8 +1,16 @@
-handle = open("mbox.txt")
+inp = input("Write mbox file path: ")
 
+try:
+    handle = open(inp)
+except:
+    print(f"File not found or cannot be opened: {inp}")
+    exit()
 
+count = 0
 for line in handle:
-    if line.startswith("From: "):
-        print(line.strip("\n"))
+    line = line.rstrip()
+    if line.startswith("Subject: "): count += 1
 
 handle.close()
+
+print(f"there were {count} subject lines in the file")
