b8243b9e05dc31b1a85ee4571ced71d129772eaf
TmSalviano <TiagoMSalviano@outlook.com>
2025-10-15 16:50:04 -0300

    Average spam confidence

--- file_handle.py
+++ file_handle.py
@@ -1,10 +1,23 @@
-
 inp = input()
+
+count = 0
+
+confidence_total = 0
 
 try:
     handle = open(inp)
+    print("File opened")
+
     for line in handle:
-        line = line.rstrip()
-        print(line.upper())
+        if not line.startswith("X-DSPAM-Confidence:") : continue
+
+        count += 1
+        first_index = line.find(":")
+        print(line[first_index + 2:])
+        confidence_total += float(line[first_index + 2:].strip())
+        print(confidence_total)
 except:
     print("Failed to open file: " + inp)
+
+average = (confidence_total / count)
+print("Average confidence: ", average)
