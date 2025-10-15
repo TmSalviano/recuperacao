a4a26df17fe1d0ba29e7167416c5102a67f6577a
TmSalviano <TiagoMSalviano@outlook.com>
2025-10-15 20:02:39 -0300

    aliasing and references in Python

--- lists.py
+++ lists.py
@@ -1,6 +1,21 @@
 
+a = "banana"
+b = "banana"
 
-handle = open("mbox.txt")
-for line in handle:
-    if not line.startswith("From ") : continue
-    print(line.split()[2])
+if a is b:
+    print("a and b point to the same object")
+
+list1 = [1, 2, 3]
+list2 = [1, 2, 3]
+
+identical = list1
+
+if list1 is list2:
+    print("list1 and 2 point to the same object'")
+else:
+    print("list1 and 2 point to different objects")
+
+if identical is list1:
+    print("The identical list not only has the same value as list1, it is identical to list it's pointing to the same object which is aliasing in python")
+
+list1[0] = 10
