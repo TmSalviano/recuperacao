e52d134eacb8f9aabb6e3c9de6a1f4538388a582
TmSalviano <TiagoMSalviano@outlook.com>
2025-10-15 16:32:13 -0300

    make all lines uppercase

--- file_handle.py
+++ file_handle.py
@@ -1,16 +1,10 @@
-inp = input("Write mbox file path: ")
+
+inp = input()
 
 try:
     handle = open(inp)
+    for line in handle:
+        line = line.rstrip()
+        print(line.upper())
 except:
-    print(f"File not found or cannot be opened: {inp}")
-    exit()
-
-count = 0
-for line in handle:
-    line = line.rstrip()
-    if line.startswith("Subject: "): count += 1
-
-handle.close()
-
-print(f"there were {count} subject lines in the file")
+    print("Failed to open file: " + inp)
