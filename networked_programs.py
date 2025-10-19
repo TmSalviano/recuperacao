518ecca0f91d25475f18e43e019a04280cfa024c
TmSalviano <TiagoMSalviano@outlook.com>
2025-10-19 19:54:19 -0300

    BeautifulSoup HTML parsing

--- networked_programs.py
+++ networked_programs.py
@@ -1,14 +1,17 @@
 import urllib.request, urllib.parse, urllib.error
+import ssl
+from bs4 import BeautifulSoup
 
-img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg')
-fhand = open("cover3.jpg", "wb")
-size = 0
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
-while True:
-    info = img.read(100000)   
-    if len(info) < 1 : break
-    size += len(info)
-    fhand.write(info)
+url = input("Enter - ")
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
 
-print(size, "characters copied")
-fhand.close()
+tags = soup("a")
+for tag in tags:
+    print("Tag ", tag)
+    print("Url: ", tag.get("href"))
+    print("Attributes: ", tag.attrs)
