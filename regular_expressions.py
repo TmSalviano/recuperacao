9e79ecb787e22aac27f4f59322ad848c608784ea
TmSalviano <TiagoMSalviano@outlook.com>
2025-10-18 23:26:55 -0300

    regular expression parsing

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,8 +1,9 @@
 import re
 
 fhandle = open("mbox-short.txt")
-
 for line in fhandle:
     line = line.rstrip()
-    if re.search("From:", line):
-        print(line)
+    f_all = re.findall("^Details: .*rev=([0-9]+)", line)
+    if len(f_all) > 0:
+        print(f_all)
+    
