1fb9f0157003e6ac421d37fa4be86c7f87c5adbe
TmSalviano <TiagoMSalviano@outlook.com>
2025-10-18 23:39:50 -0300

    parse hour in line simplified with regular expressions

--- regular_expressions.py
+++ regular_expressions.py
@@ -3,7 +3,7 @@ import re
 fhandle = open("mbox-short.txt")
 for line in fhandle:
     line = line.rstrip()
-    f_all = re.findall("^Details: .*rev=([0-9]+)", line)
+    f_all = re.findall("^From .+ ([0-9][0-9]):", line)
     if len(f_all) > 0:
         print(f_all)
     
