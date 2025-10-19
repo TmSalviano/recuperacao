66201878f4fa8cf333689ec65983bc886d15b6ea
TmSalviano <TiagoMSalviano@outlook.com>
2025-10-19 13:32:29 -0300

    add: new revision average calculator

---
+++ new_revision_average.py
@@ -0,0 +1,22 @@
+import re
+
+i = input("Enter a file: ")
+try:
+    fhandle = open(i)
+except:
+    print("Failed to open file %s" % i)
+
+count = 0
+
+t = list()
+
+for line in fhandle:
+    line = line.rstrip()
+    l = re.findall("^New Revision: ([0-9]+)", line)
+    if len(l) > 0:
+        for s in l:
+            t.append(int(s))
+        count += 1
+
+
+print((sum(t) // count))
--- regular_expressions.py
+++
@@ -1,8 +0,0 @@
-import re
-
-fhandle = open("mbox-short.txt")
-for line in fhandle:
-    line = line.rstrip()
-    f_all = re.findall("^SAK-([0-9]+)", line)
-    if len(f_all) > 0:
-        print(f_all)
